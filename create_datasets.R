fa534c3ac59783644e2aa36b4779f966773da950
joyce gan <xiaoying@dhcp-vl2041-20940.eduroam.cornell.edu>
2024-04-18 15:23:29 -0400

    initial file

---
+++ create_datasets.R
@@ -0,0 +1,15 @@
+# figure out how to read datasets efficiently and cleanly while making these
+# summaries at the same time
+
+# write final dataset into a file in rustcrn (I like that name it's dumb)
+
+# A dataset with one row for each station, providing information about the
+# station identifier, station name, state, longitude, and latitude.
+
+# The full daily dataset with the following columns:
+#  • WBANNO, state, station name, LST_DATE, CRX_VN, LONGITUDE, LATITUDE,
+#    T_DAILY_MAX, T_DAILY_MIN, T_DAILY_MEAN, T_DAILY_AVG, P_DAILY_CALC,
+#    SOLARAD_DAILY
+# Convert missing value codes into NAs. The date column should be in R’s date
+# format. Be sure to document your dataset, explaining what each column means.
+
