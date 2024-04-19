ab6341b8de6b8552602b7913a59183a8fb43d8d7
jxgan <joyce.gan2@gmail.com>
2024-04-19 17:00:29 -0400

    dataset creation

--- create_datasets.R
+++ create_datasets.R
@@ -1,7 +1,4 @@
-# figure out how to read datasets efficiently and cleanly while making these
-# summaries at the same time
-
-# write final dataset into a file in rustcrn
+# TODO: document datasets
 
 # A dataset with one row for each station, providing information about the
 # station identifier, station name, state, longitude, and latitude.
@@ -13,3 +10,64 @@
 # Convert missing value codes into NAs. The date column should be in R’s date
 # format. Be sure to document your dataset, explaining what each column means.
 
+# Reads raw data, organized in folders by year
+raw_names <- list.files(path = "raw_data",
+                        recursive = TRUE,
+                        full.names = TRUE)
+# Reads header names
+headers <- read.table("headers.txt")[29:56,]
+
+# Read in raw data to a list of data frames
+raw_data <- lapply( raw_names,
+                    function(x) { read.table(x, col.names = headers) } )
+# Name each list element with the file path
+names(raw_data) <- raw_names
+
+# Define names for full dataset
+full_names <- c("WBANNO", "state", "station_name", "LST_DATE", "CRX_VN",
+                "LONGITUDE", "LATITUDE",   "T_DAILY_MAX", "T_DAILY_MIN",
+                "T_DAILY_MEAN", "T_DAILY_AVG", "P_DAILY_CALC", "SOLARAD_DAILY")
+
+# Get how many rows are in each individual dataset + total rows
+rows <- sapply(raw_data, function(x) {nrow(x)})
+total_rows <- sum(rows)
+
+# Create base data frame for full data set
+full_data <- as.data.frame(matrix(NA, nrow = total_rows,
+                                  ncol = length(full_names),
+                                  dimnames = list(1:total_rows, full_names)))
+
+# Loop through rows, adding data from each data frame into full data set
+j <- 1
+for(i in 1:total_rows) {
+  raw <- raw_data[[i]]
+
+  raw <- cbind(raw["WBANNO"],
+               state = substring(raw_names[i], 29, 30),
+               station_name =
+                 substring(raw_names[i], 32, nchar(raw_names[i]) - 4),
+               raw[,2:11])
+
+  full_data[j:(j + rows[i] - 1),] <- raw
+  j <- j + rows[i]
+}
+
+# Replace missing values with NA
+full_data[full_data == -9999] <- NA
+
+# Convert dates to Date objects
+dates <- as.Date(as.character(full_data$LST_DATE), format = "%Y%m%d")
+full_data$LST_DATE <- dates
+
+# Write full dataset to file
+saveRDS(full_data, file = "data/full_daily_weather.rds")
+
+# Collect individual station data by station identifier
+station_data <- full_data[!duplicated(full_data["WBANNO"]),][,c("WBANNO",
+                                                                "state",
+                                                                "station_name",
+                                                                "LONGITUDE",
+                                                                "LATITUDE")]
+
+# Write station data to file
+saveRDS(station_data, file = "data/stations.rds")
