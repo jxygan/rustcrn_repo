d8de19fc24a708918ba19cbeb199c17d739ac80e
jxgan <joyce.gan2@gmail.com>
2024-04-25 15:36:59 -0400

    minor edits

--- rustcrn/R/station_analysis.R
+++ rustcrn/R/station_analysis.R
@@ -13,18 +13,18 @@
 #' @examples
 time_series <- function(station_id, start_date = NULL, end_date = NULL) {
   # Filter data by station ID first
-  station_data <- full_daily_weather %>% dplyr::filter(WBANNO == station_id)
+  station_data <- full_daily_weather |> dplyr::filter(WBANNO == station_id)
 
   # Check and apply start date filter if provided
   if (!is.null(start_date)) {
     start_date <- as.Date(start_date)
-    station_data <- station_data %>% dplyr::filter(LST_DATE >= start_date)
+    station_data <- station_data |> dplyr::filter(LST_DATE >= start_date)
   }
 
   # Check and apply end date filter if provided
   if (!is.null(end_date)) {
     end_date <- as.Date(end_date)
-    station_data <- station_data %>% dplyr::filter(LST_DATE <= end_date)
+    station_data <- station_data |> dplyr::filter(LST_DATE <= end_date)
   }
 
   return(station_data)
@@ -37,7 +37,7 @@ time_series <- function(station_id, start_date = NULL, end_date = NULL) {
 #'
 #' @param station_id The station ID (WBANNO number)
 #'
-#' @return A data frame containing two columns for the day of year (0-365) and predicted average temperatures for each day, in degrees Celsius.
+#' @return A data frame containing two columns for the day of year (1-365) and predicted average temperatures for each day, in degrees Celsius.
 #' @export
 #'
 #' @examples
@@ -47,11 +47,12 @@ yearly_cycle <- function(station_id) {
   ts$doy <- as.POSIXlt(ts$LST_DATE)$yday
 
   # Fit sinusoidal model to average temperature data
+  # Fix with different wave function? some data not so smooth?
   year.lm <- lm(T_DAILY_AVG ~ cos(2*pi*doy/365) + sin(2*pi*doy/365),
                 data = ts)
 
-  preds <- predict(year.lm, newdata = list(doy = 0:365))
+  preds <- predict(year.lm, newdata = list(doy = 1:365))
 
   # Return data frame with columns for DOY and predictions
-  return(data.frame(doy = 0:365, t_daily_pred = preds))
+  return(data.frame(doy = 1:365, t_daily_pred = preds))
 }
