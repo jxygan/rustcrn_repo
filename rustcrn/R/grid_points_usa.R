88663ba4579ff6ce03d19f4ccc056ab507bb8f1e
Crystal Lim <crystallim@dhcp-vl2042-16659.redrover.cornell.edu>
2024-05-03 12:48:20 -0400

    edits

--- rustcrn/R/grid_points_usa.R
+++ rustcrn/R/grid_points_usa.R
@@ -1,33 +1,43 @@
+#'Create a grid of points that fall within the contiguous USA
+#'
+#'Get a data frame of grid points. Each row corresponding to a specific grid point,
+#'showing its longitude, latitude, and whether it's inside the USA by binary.
+#'
+#'
+#' @param resolution numeric value for resolution of map
+#'
+#' @return A dataframe of grid points with longitude, latitude, and whether it's inside the USA
+#' @export
+#'
+#' @example
+#' #grid with resolution 30
+#' grid_points_usa(30)
+#'
+#' #grid with resolution 100
+#' grid_points_usa()
 
-#####################
-
-grid_points_usa <- function(resolution){
-
-  # Get map data for the USA
+grid_points_usa <- function(resolution = 100){
+  
+  # Gets map data for the USA
   usa_map <- ggplot2::map_data("usa")
   usa_map <- usa_map[usa_map$region == "main", ]
-
-  #square matrix of min max coordinates
-  lons <- seq(min(usa_map$long), max(usa_map$long), length.out = resolution)
-  lats <- seq(min(usa_map$lat), max(usa_map$lat), length.out = resolution)
+  
+  #Square matrix of min max coordinates and resolution
+  lons <- seq(min(usa_map$long),max(usa_map$long), length.out = resolution)
+  lats <- seq(min(usa_map$lat),max(usa_map$lat), length.out = resolution)
   grid_square <- expand.grid(lons, lats)
-
-  #find intersection points
+  
+  #Find points that overlap
   intersection <- sp::point.in.polygon(grid_square$Var1,grid_square$Var2,
                                        usa_map$long, usa_map$lat )
-
-
-  #Use intersection points to show if coordinates in USA location
+  
+  
+  #Use intersection to show if coordinates in USA location by 1,0
   grid_square$in_us <- intersection
+  
+  #Rename columns to fit longitude latitude.
   names(grid_square)[1] <- "long"
   names(grid_square)[2] <- "lat"
-
+  
   return(grid_square)
 }
-
-#returns a row of all grid points and if it is in USA
-
-#test
-# result <- grid_points_usa(100)
-# result
-
--- rustcrn/R/plotting_intrapolation.R
+++ rustcrn/R/plotting_intrapolation.R
@@ -1,20 +1,47 @@
-
-################################ 9
-
-#the input is the interpolated data and grid points of all (including us and not)
-
+#'Plot the intrapolated data
+#'
+#'Using the intrapolated data, produces a visual representation of predicted
+#'results by plotting through image.plot
+#'
+#' @param data data frame of US longitude, US latitude, wheather it's inside the
+#'  USA (1,0), and predicted values
+#'
+#' @return Produce visual representation of spatial predictions
+#' @export
+#'
+#' @example
+#' plotting_interpolation(data)
+#'
 plotting_interpolation <- function(data) {
-
+  
   #make a matrix of the data
-  pred.matrix <- matrix(data, nrow = length(data$long), ncol = length(data$lat),
-                        dimnames = list(lons, lats))
-
-  imgX <- list(x = lons, y = lats, z = pred.matrix)
-
+  pred.matrix <- matrix(data[4], nrow = length(data$long), ncol = length(data$lat),
+                        dimnames = list(long, lats))
+  
+  imgX <- list(x = lons, y= lats, z = pred.matrix)
+  
   #plot the intrapolation
   xline <- unique(data$long)
   yline <- unique(data$lat)
-  fields::image.plot( xline, yline , imgX, xlab = "Longitude" , ylab = "Latitude",
-                      main = "Predicted vallue by location in US" )
-
+  
+  for (i in 1:nrow(data)) {
+    if (data$in_us[i] == 1) {  # Check if the point is in the US
+      fields::image.plot(xline, yline, imgX$z,
+                         xlab = "Longitude", ylab = "Latitude",
+                         main = "Predicted Value by Location in US",
+                         col = fields::tim.colors(256),
+                         xaxt = "n", yaxt = "n",
+                         xlim = range(xline), ylim = range(yline),
+                         cex.main = 1.5,
+                         cex.lab = 1.2,
+                         cex.axis = 1.1,
+                         key.title = "Predicted Value",
+                         key.axes = axis(1, at = pretty(imgX$z), labels = format(pretty(imgX$z), nsmall = 2))  # Custom key axis
+      )
+      axis(1, at = pretty(xline), labels = format(pretty(xline), nsmall = 2))
+      axis(2, at = pretty(yline), labels = format(pretty(yline), nsmall = 2))
+      box()
+      break
+    }
+  }
 }
